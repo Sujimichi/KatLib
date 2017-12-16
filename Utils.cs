60c4325fa61960f6ecc97be749f1bcc87cb555e0
Sujimichi <sujimichi@gmail.com>
2017-12-16 09:55:48 +0000

    better OS path joining

--- Utils.cs
+++ Utils.cs
@@ -72,16 +72,23 @@ namespace KatLib
 
     public class Paths
     {
-        //takes any number of strings and returns them joined together with OS specific path divider, ie:
-        //Paths.joined("follow", "the", "yellow", "brick", "road") -> "follow/the/yellow/brick/road or follow\the\yellow\brick\road
-        //actually, not true, now it just joins them with /, as it seems mono sorts out path dividers anyway and using \ just messes things up here. (I mean, what kinda os uses \ anyway, madness).
+        //takes any number of strings and returns them joined together with Linux specific path divider, ie:
+        //Paths.joined("follow", "the", "yellow", "brick", "road") -> "follow/the/yellow/brick/road 
         static public string joined(params string[] paths){
-            string path = paths[0];
-            for(int i = 1; i < paths.Length; i++){
-                path = path + "/" + paths[i];
-            }
-            return path;
+            return String.Join("/", paths).Replace("\\", "/");
         }
+
+        //takes any number of strings and returns them joined together with OS specific path divider, ie:
+        //Paths.joined("follow", "the", "yellow", "brick", "road") -> "follow/the/yellow/brick/road or follow\the\yellow\brick\road (I mean, what kinda os uses \ anyway, madness).
+        static public string os_joined(params string[] paths){
+            return String.Join(Path.DirectorySeparatorChar.ToString(), paths);
+        }
+
+        static public string os_safe(string path_string){
+            path_string = path_string.Replace('\\', '/');
+            return path_string.Replace('/', Path.DirectorySeparatorChar);
+        }
+
     }
 
 
